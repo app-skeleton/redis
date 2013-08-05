5de680c7bd9e6bfc483cb6761972993108a54b39
Pap Tamas <paptamas@gmail.com>
2013-08-05 14:03:24 +0300

    Implement the Redis_Exception class.

---
+++ classes/Kohana/Redis/Exception.php
@@ -0,0 +1,15 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/*
+ * @package		Redis Module
+ * @author      Pap Tamas
+ * @copyright   (c) 2011-2013 Pap Tamas
+ * @website		https://bitbucket.org/paptamas/kohana-redis
+ * @license		http://www.opensource.org/licenses/isc-license.txt
+ *
+ */
+
+class Kohana_Redis_Exception extends Kohana_Exception {
+
+}
+
+// END Kohana_Redis_Exception
---
+++ classes/Redis/Exception.php
@@ -0,0 +1,15 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/*
+ * @package		Redis Module
+ * @author      Pap Tamas
+ * @copyright   (c) 2011-2013 Pap Tamas
+ * @website		https://bitbucket.org/paptamas/kohana-redis
+ * @license		http://www.opensource.org/licenses/isc-license.txt
+ *
+ */
+
+class Redis_Exception extends Kohana_Redis_Exception {
+
+}
+
+// END Redis_Exception
